1394c61327a5ff662eb416f6a2241c05445bb2df
Rakesh Kumar <rakeshrajput537@gmail.com>
2020-01-12 11:58:37 +0530

    update

--- src/navBar.js
+++ src/navBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import "./navBar.css";
+import "./navBar.scss";
 export default function NavBar(N) {
     return(
         <div className="navB" id="navbar">
             <div className="ullist">
-                <spam className="lilist" ><a href="#home"><b style={{color:"yellow"}}>KHANAKHOJO</b></a></spam>
-                <spam className="lilist"><a href="#news">News</a></spam>
-                <spam className="lilist"><a href="#contact">Contact</a></spam>
-                <spam className="lilist" style={{float:"right"}}><a className="active" href="#about">Login</a></spam>
+                <spam className="brand" ><b>KHANA<strong>KHOJO</strong></b></spam>
+                {/* <spam className="lilist">BMI</spam>
+                <spam className="lilist"><a href="#contact">Item</a></spam> */}
+                <spam className="lilist" style={{float:"right"}}><a className="active" href="#about"><strong>Login</strong></a></spam>
             </div>
 
         </div>
